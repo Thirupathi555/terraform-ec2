7c41219d59802a779c1b865d0ac4e700a4253346
Thirupathi555 <thirupathisee339@gmail.com>
2025-01-25 14:20:56 +0530

    ec2

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,14 @@
 resource "aws_instance" "terraform" {
+<<<<<<< HEAD
 
     ami = var.ami_id  # this AMI ID may change over the time 
     instance_type = var.instance_type
     vpc_security_group_ids = var.security_group_ids  # we used allow-all security group ID here 
 }
+=======
+    count = var
+    ami = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = var.security_group_ids
+}
+>>>>>>> f7910b1 (ec2)
